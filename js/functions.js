22ba0e978461e6ced07b253f38c1f17463794fee
franco-acosta <121576422+franco-acosta@users.noreply.github.com>
2023-02-02 15:11:15 -0300

    Archivos necesarios de la página.

---
+++ js/functions.js
@@ -0,0 +1,34 @@
+/************* Función Aside **************/
+
+document.getElementById('display-educ').addEventListener('click', function() {
+    if (document.getElementById('hidden1').style.display == "" || document.getElementById('hidden1').style.display == 'none') {
+        document.getElementById('hidden1').style.display = 'block';
+    } 
+    else { document.getElementById('hidden1').style.display = 'none';
+    }
+})
+
+document.getElementById('display-educ').addEventListener('click', function() {
+    if (document.getElementById('hidden2').style.display == "" || document.getElementById('hidden2').style.display == 'none') {
+        document.getElementById('hidden2').style.display = 'block'; 
+    } 
+    else { document.getElementById('hidden2').style.display = 'none';
+    }
+})
+
+document.getElementById('display-ref').addEventListener('click', function() {
+    if (document.getElementById('hidden3').style.display == "" || document.getElementById('hidden3').style.display == 'none') {
+        document.getElementById('hidden3').style.display = 'block';
+    } 
+    else { document.getElementById('hidden3').style.display = 'none';
+    }
+})
+
+document.getElementById('display-ref').addEventListener('click', function() {
+    if (document.getElementById('hidden4').style.display == "" || document.getElementById('hidden4').style.display == 'none') {
+        document.getElementById('hidden4').style.display = 'block';
+    } 
+    else { document.getElementById('hidden4').style.display = 'none';
+    }
+})
+/******************************************/
